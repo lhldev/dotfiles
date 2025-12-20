052c29aab81a19856866c10a780cf777599a861a
Hoonlim Lee <hoonlimlee@gmail.com>
2025-12-20 20:07:18 +0800

    Added theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
 	require("plugins.treesitter"),
 	require("plugins.lsp"),
 	require("plugins.blink-cmp"),
+	require("plugins.color"),
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
