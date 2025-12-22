3c678ec73d2e7bb8716913854b5929e31c6bc173
Hoonlim Lee <hoonlimlee@gmail.com>
2025-12-22 08:59:41 +0800

    Added harpoon

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
 	require("plugins.indent-blankline"),
 	require("plugins.fugitive"),
 	require("plugins.undotree"),
+	require("plugins.harpoon"),
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,12 +1,28 @@
-local harpoon = require("harpoon")
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():add()
+			end, { desc = "Harpoon: Add file" })
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end, { desc = "Harpoon: Menu" })
 
-harpoon:setup()
+			-- Loop for files 1-9
+			for i = 1, 9 do
+				vim.keymap.set("n", "<leader>" .. i, function()
+					harpoon:list():select(i)
+				end, { desc = "Harpoon: Select " .. i })
+			end
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-for i = 1, 9 do
-    vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end, { desc = "Harpoon: Select File " .. i })
-end
-
-vim.keymap.set("n", "<leader>0", function() harpoon:list():select(10) end, { desc = "Harpoon: Select File 10" })
+			-- File 10 (mapped to 0)
+			vim.keymap.set("n", "<leader>0", function()
+				harpoon:list():select(10)
+			end, { desc = "Harpoon: Select 10" })
+		end,
+	},
+}
