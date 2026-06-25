886fedf0aec778470109e6aa1acdf2be5f68f4bb
Hoonlim Lee <hoonlimlee@gmail.com>
2026-06-25 22:40:46 +0800

    Removed unused lines

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -240,7 +240,6 @@ return {
 						settings = {
 							-- Rebuild on write until zls gets better.
 							enable_build_on_save = true,
-							build_on_save_step = "check",
 						},
 					},
 					pyright = {},
