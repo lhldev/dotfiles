c236601006e5b1ef92bbdebca87d27c66b246bcc
Hoonlim Lee <hoonlimlee@gmail.com>
2026-07-11 10:48:30 +0800

    New color scheme

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -4,15 +4,17 @@ return {
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"olimorris/onedarkpro.nvim",
+		"ellisonleao/gruvbox.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		config = function()
-
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("onedark_dark")
-            vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1a1a1a" })
+			require("gruvbox").setup({
+				contrast = "hard", -- Optional: "hard", "soft", or "" for standard contrast
+			})
+			vim.cmd.colorscheme("gruvbox")
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1a1a1a" })
 		end,
 	},
 }
