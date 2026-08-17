f34d8340686843dfbbba7739adce53285d7aa3db
Hoonlim Lee <hoonlimlee@gmail.com>
2026-08-17 15:37:04 +0800

    Added flash plugin to nvim

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
 	require("plugins.fugitive"),
 	require("plugins.undotree"),
 	require("plugins.harpoon"),
+	require("plugins.flash"),
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
